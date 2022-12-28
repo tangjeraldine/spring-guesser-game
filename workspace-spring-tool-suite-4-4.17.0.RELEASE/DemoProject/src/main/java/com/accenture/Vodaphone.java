f6fde58f141cef5a3bde37e17cc6a1c2cded21c8
tangjeraldine <typjera.1127@gmail.com>
2022-12-29 00:54:53 +0800

    Added beans and studied instantiation of singleton and prototype scopes. First commit.

---
+++ workspace-spring-tool-suite-4-4.17.0.RELEASE/DemoProject/src/main/java/com/accenture/Vodaphone.java
@@ -0,0 +1,11 @@
+package com.accenture;
+
+public class Vodaphone {
+	public Vodaphone() {
+		System.out.println("Vodaphone Object Instantiated...");
+	}
+	
+	public void test() {
+		System.out.println("Vodaphone network testing done...");
+	}
+}
