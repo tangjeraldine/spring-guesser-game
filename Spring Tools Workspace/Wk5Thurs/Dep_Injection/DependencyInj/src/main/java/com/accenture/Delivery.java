560c2882c675f2ac2afd0aef9ac2742092e3fd79
tangjeraldine <103403805+tangjeraldine@users.noreply.github.com>
2022-12-29 23:52:07 +0800

    Delete Spring Tools Workspace/Wk5Thurs/Dep_Injection/DependencyInj directory

--- Spring Tools Workspace/Wk5Thurs/Dep_Injection/DependencyInj/src/main/java/com/accenture/Delivery.java
+++
@@ -1,8 +0,0 @@
-package com.accenture;
-
-import org.springframework.stereotype.Component;
-
-//@Component("delivery")
-public class Delivery {
-	// create a bean with id of "delivery" using the @Component annotation
-}
--- Spring Tools Workspace/Wk5Thurs/Dep_Injection/DependencyInj/src/main/java/com/accenture/Deliveryman.java
+++
@@ -1,7 +0,0 @@
-package com.accenture;
-
-public class Deliveryman {
-	public void info() {
-		System.out.println("Hello! I am from DHL. Your delivery is here!");
-	}
-}
