0f25e779b8a3057b3aa052471556656633b46b20
tangjeraldine <typjera.1127@gmail.com>
2022-12-29 23:50:49 +0800

    Tested functionality of game - success

--- Spring Tools Workspace/Wk5Thurs/Guesser_Game/Guesser_Game/src/main/java/com/accenture/App.java
+++ Spring Tools Workspace/Wk5Thurs/Guesser_Game/Guesser_Game/src/main/java/com/accenture/App.java
@@ -3,19 +3,18 @@ package com.accenture;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
-    	Guesser guesser = (Guesser) context.getBean("guesser");
-    	Player p1 = (Player) context.getBean("player");
-    	Player p2 = (Player) context.getBean("player");
-    	Player p3 = (Player) context.getBean("player");
-    	Umpire umpire = (Umpire) context.getBean("umpire");
-    	
-    	umpire.collectFromGuesser(guesser);
-    	umpire.collectFromPlayers(p1, p2, p3);
-    	umpire.compareNums();
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+
+		Guesser guesser = (Guesser) context.getBean("guesser");
+		Player p1 = (Player) context.getBean("player");
+		Player p2 = (Player) context.getBean("player");
+		Player p3 = (Player) context.getBean("player");
+		Umpire umpire = (Umpire) context.getBean("umpire");
+
+		umpire.collectFromGuesser(guesser);
+		umpire.collectFromPlayers(p1, p2, p3);
+		umpire.compareNums();
+	}
 }
